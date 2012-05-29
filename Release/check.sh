65a617cacaed0b7ba9e8337c5e2775aa9d430c60
secrule <falcon@secrule.com>
2012-05-29 13:44:48 +0800

    check.sh update

--- Release/check.sh
+++ Release/check.sh
@@ -8,6 +8,14 @@ else
 	exit 1
 fi
 
+if [ -d /usr/local/include/inotifytools ]
+then
+	echo "Found inotifytools success!"
+else
+	echo "inotifytools not found!Plz install inotifytools first"
+	exit 1
+fi
+
 if [ -d /usr/include/mysql ] || [ -d /usr/lib/mysql ]
 then
 	echo "Found mysql-dev environment success!"
